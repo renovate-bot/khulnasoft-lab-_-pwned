222d78cee0887e456acfccf59d6353ad966ce587
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-09-05 04:20:46 +0600

    Create logger.test.ts

---
+++ src/utils/__tests__/logger.test.ts
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+import { vi, type SpyInstance } from 'vitest';
+import { loggerFns } from '../../../test/fixtures.js';
+import { logger, type Logger, type LoggerFunction } from '../logger.js';
+
+type IndexableConsole = typeof console &
+  Record<string, LoggerFunction | SpyInstance>;
+type IndexableLogger = Logger & Record<string, LoggerFunction>;
+
+const indexableConsole = console as IndexableConsole;
+const indexableLogger = logger as IndexableLogger;
+
+describe('util: logger', () => {
+  it('returns an object with all the expected methods', () => {
+    expect(logger).toEqual(
+      expect.objectContaining(
+        loggerFns.reduce(
+          (obj, fn) => ({
+            ...obj,
+            [fn]: expect.any(Function) as unknown,
+          }),
+          {},
+        ),
+      ),
+    );
+  });
+
+  it('calls the corresponding console functions with the same arguments', () => {
+    const args = ['Wubba lubba dub dub!', { param: 'value' }];
+    loggerFns.forEach((fn) => {
+      const orig = indexableConsole[fn];
+      indexableConsole[fn] = vi.fn();
+      expect(indexableConsole[fn]).toHaveBeenCalledTimes(0);
+      indexableLogger[fn](...args);
+      expect(indexableConsole[fn]).toHaveBeenCalledTimes(1);
+      expect(indexableConsole[fn]).toHaveBeenCalledWith(...args);
+      indexableConsole[fn] = orig;
+    });
+  });
+});
