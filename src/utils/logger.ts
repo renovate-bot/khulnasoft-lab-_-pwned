fc2d1ae313e5250d6f8f7178e5e68c9e80e6bbd2
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-09-05 04:19:32 +0600

    Create logger.ts

---
+++ src/utils/logger.ts
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+export type LoggerFunction = (message?: any, ...args: any[]) => void;
+
+export interface Logger {
+  error: LoggerFunction;
+  info: LoggerFunction;
+  log: LoggerFunction;
+  warn: LoggerFunction;
+}
+
+/**
+ * Default console logging wrapper to facilitate mocking/suppressing output
+ * during tests.
+ */
+export const logger: Logger = {
+  error: (...args) => {
+    console.error(...args);
+  },
+  info: (...args) => {
+    console.info(...args);
+  },
+  log: (...args) => {
+    console.log(...args);
+  },
+  warn: (...args) => {
+    console.warn(...args);
+  },
+};
